694fe06222cccc7600f77a94d2a7484224d03123
Meowfessor96 <155883326+Meowfessor96@users.noreply.github.com>
2024-02-27 11:53:37 +0530

    Update data.c

--- 26/codes/data.c
+++ 26/codes/data.c
@@ -4,7 +4,6 @@ int main() {
     int n;
     double y;
 
-    // Open the file for writing
     FILE *fp = fopen("data.dat", "w");
 
     if (fp == NULL) {
@@ -12,13 +11,11 @@ int main() {
         return 1;
     }
 
-    // Write the header
     fprintf(fp, "n\ty\n");
 	y = 1 ;
-    // Generate the first 10 values and write to the file
     for (n = 0; n < 10; n++) {
+	fprintf(fp, "%d\t%.5f\n", n, y);
         y = (n >= 0) ? y*0.5 : 0; // Apply unit step function
-        fprintf(fp, "%d\t%.5f\n", n, y);
     }
 
     // Close the file
